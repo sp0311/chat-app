948ebe7580cb37d7398eec567583945f0ae7e6db
sp0311 <qploop@yahoo.co.jp>
2020-07-27 11:40:30 +0900

    各モデルのテスト

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,10 @@
-{"authenticity_token"=>"vMBN4eydYeQJXDm95CdKN9Ve0qbhTv0XsN+1bTDtKSrokw+JjdmTD9V1RYyn4pvyibi2tZwIkBWeZP5Coh682A=="def method_name
+
+# メンタさんのメモ？{"authenticity_token"=>"vMBN4eydYeQJXDm95CdKN9Ve0qbhTv0XsN+1bTDtKSrokw+JjdmTD9V1RYyn4pvyibi2tZwIkBWeZP5Coh682A=="def method_name
   
-end "room"=>{"user-ids"=>["1"]def method_name
+# end "room"=>{"user-ids"=>["1"]def method_name
   
-end "name"=>"cat"def method_name
+# end "name"=>"cat"def method_name
   
-end "user_ids"=>[""]}def method_name
+# end "user_ids"=>[""]}def method_name
   
-end "commit"=>"Create Room"}
+# end "commit"=>"Create Room"}
